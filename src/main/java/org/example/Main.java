d455b2af358a81f2eb680a75b1f3a3ca2bf19cd3
sabina1892 <sabinamammadova@hotmail.com>
2022-10-12 11:28:51 +0400

    third commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,5 +4,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("hi");
+        System.out.println("hhhhi");
+
     }
 }
