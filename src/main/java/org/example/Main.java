55cc4cbba26292884ab47babb5c649215d7c426e
telman03 <telmangadimov1@gmail.com>
2022-10-12 11:38:11 +0400

    test commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,6 +5,7 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("hi");
         System.out.println("hhhhi");
+        System.out.println("sa");
 
     }
 }
