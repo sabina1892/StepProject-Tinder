d0eaf2502e542f91b582f8fa08bd202813e6b5cd
telman03 <telmangadimov1@gmail.com>
2022-10-12 11:44:20 +0400

    ServerApp.java added

--- src/main/java/tinder/ServerApp.java
+++ src/main/java/tinder/ServerApp.java
@@ -1,4 +1,12 @@
 package tinder;
 
+import org.eclipse.jetty.server.Server;
+
 public class ServerApp {
+    public static void main(String[] args) throws Exception {
+        Server server = new Server(8080);
+
+        server.start();
+        server.join();
+    }
 }
